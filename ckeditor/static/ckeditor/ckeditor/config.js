5cdad9af5cc35e7ea540edf8617e0b705e23e9a7
roman <rgorkovenko@gmail.com>
2016-02-09 21:17:06 +0600

    added link to youtube

--- ckeditor/static/ckeditor/ckeditor/config.js
+++ ckeditor/static/ckeditor/ckeditor/config.js
@@ -7,5 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'youtube';
 };
+
+console.log(123);
 
