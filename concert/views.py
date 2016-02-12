81c0b082484c4cf7056e080695d7628dd97f071e
slavaaf <crazzy.dead@gmail.com>
2016-02-12 19:16:56 +0300

    test commit url

--- concert/views.py
+++ concert/views.py
@@ -9,3 +9,9 @@ def index_view(request):
     object_list = []
     object_list = Post.objects.all()
     return render(request, 'index.html', {'object_list': object_list})
+
+
+# вьюха вот, тестовая
+def contact_view(request):
+    object_list = Post.objects.all()
+    return render(request, 'contact.html', {'object_list': object_list})
--- kubric_club/urls.py
+++ kubric_club/urls.py
@@ -24,6 +24,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', index_view, name='index'),
+    # ссылка вот
+    url(r'^contacts/$', contact_view, name='contact'),
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
