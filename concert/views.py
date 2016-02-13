33b9b921df1188a7f9dd8fa6fb6ac976d360eca2
roman <rgorkovenko@gmail.com>
2016-02-13 17:20:05 +0600

    move condition to server which limits count records

--- concert/views.py
+++ concert/views.py
@@ -10,14 +10,17 @@ def index_view(request):
     object_list = Post.objects.all()
     result_list = []
     for obj in object_list:
-        result_obj = {'event_type': obj.event_type, 'datetime': obj.datetime,
-                      'preview_image': obj.preview_image, 'title': obj.title, 'preview_text': obj.preview_text,
-                      'ticket_price': obj.ticket_price, 'ticket_vip_price': obj.ticket_vip_price,
-                      'ticket_link': obj.ticket_link,
-                      'actual': True if datetime.datetime(obj.datetime.year, obj.datetime.month,
-                                                          obj.datetime.day, obj.datetime.hour,
-                                                          obj.datetime.minute) > datetime.datetime.now() else False}
-        result_list.append(result_obj)
+        actual = True if datetime.datetime(obj.datetime.year, obj.datetime.month,
+                                           obj.datetime.day, obj.datetime.hour,
+                                           obj.datetime.minute) > datetime.datetime.now() else False
+        if actual:
+            result_obj = {'event_type': obj.event_type, 'datetime': obj.datetime,
+                          'preview_image': obj.preview_image, 'title': obj.title, 'preview_text': obj.preview_text,
+                          'ticket_price': obj.ticket_price, 'ticket_vip_price': obj.ticket_vip_price,
+                          'ticket_link': obj.ticket_link,
+                          }
+
+            result_list.append(result_obj)
     return render(request, 'index.html', {'object_list': result_list, 'now': datetime.datetime.now()})
 
 
