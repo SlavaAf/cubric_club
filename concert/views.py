d38fbb1b741228b18314df6f73a584bc42425035
slavaaf <crazzy.dead@gmail.com>
2016-02-18 17:50:11 +0300

    fix evenet detail

--- concert/views.py
+++ concert/views.py
@@ -21,21 +21,60 @@ def index_view(request):
                           }
 
             result_list.append(result_obj)
-    return render(request, 'index.html', {'object_list': result_list,
-                                          'now': datetime.datetime.now()})
+    return render(request, 'index.html', {'object_list': result_list})
 
 
 def contact_view(request):
-    object_list = []
-    return render(request, 'contact.html', {'object_list': object_list})
+    object_list = Post.objects.all()
+    result_list = []
+    for obj in object_list:
+        actual = True if datetime.datetime(obj.datetime.year, obj.datetime.month,
+                                           obj.datetime.day, obj.datetime.hour,
+                                           obj.datetime.minute) > datetime.datetime.now() else False
+        if actual:
+            result_obj = {'event_type': obj.event_type, 'datetime': obj.datetime,
+                          'preview_image': obj.preview_image, 'title': obj.title, 'preview_text': obj.preview_text,
+                          'ticket_price': obj.ticket_price, 'ticket_vip_price': obj.ticket_vip_price,
+                          'ticket_link': obj.ticket_link, 'pk': obj.pk,
+                          }
+
+            result_list.append(result_obj)
+    return render(request, 'contact.html', {'object_list': result_list})
 
 
 def about_view(request):
     object_list = Post.objects.all()
-    return render(request, 'about.html', {'object_list': object_list})
+    result_list = []
+    for obj in object_list:
+        actual = True if datetime.datetime(obj.datetime.year, obj.datetime.month,
+                                           obj.datetime.day, obj.datetime.hour,
+                                           obj.datetime.minute) > datetime.datetime.now() else False
+        if actual:
+            result_obj = {'event_type': obj.event_type, 'datetime': obj.datetime,
+                          'preview_image': obj.preview_image, 'title': obj.title, 'preview_text': obj.preview_text,
+                          'ticket_price': obj.ticket_price, 'ticket_vip_price': obj.ticket_vip_price,
+                          'ticket_link': obj.ticket_link, 'pk': obj.pk,
+                          }
+
+            result_list.append(result_obj)
+    return render(request, 'about.html', {'object_list': result_list})
 
 
 def detail_view(request, pk):
     event = Post.objects.get(pk=pk)
-    return render(request, 'event_detail.html', {'event': event})
+    object_list = Post.objects.all()
+    result_list = []
+    for obj in object_list:
+        actual = True if datetime.datetime(obj.datetime.year, obj.datetime.month,
+                                           obj.datetime.day, obj.datetime.hour,
+                                           obj.datetime.minute) > datetime.datetime.now() else False
+        if actual:
+            result_obj = {'event_type': obj.event_type, 'datetime': obj.datetime,
+                          'preview_image': obj.preview_image, 'title': obj.title, 'preview_text': obj.preview_text,
+                          'ticket_price': obj.ticket_price, 'ticket_vip_price': obj.ticket_vip_price,
+                          'ticket_link': obj.ticket_link, 'pk': obj.pk,
+                          }
+
+            result_list.append(result_obj)
+    return render(request, 'event_detail.html', {'event': event, 'object_list': result_list})
 
